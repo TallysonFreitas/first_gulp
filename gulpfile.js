854921b92d7ddab0eeff1dbe402246dfbba50a07
Tallyson Freitas <tallyson.eu@outlook.com>
2023-06-17 15:01:44 -0300

    altomacoes com gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
 const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const sourcemaps = require('gulp-sourcemaps')
 
+
+function compilaSass() { 
+    return gulp.src('./source/styles/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle:'compressed'
+        }))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./build/styles'))
+}
 
 function funcaoPadrao(callback){
     setTimeout(function(){
@@ -23,3 +35,7 @@ function dizTchau(){
 exports.series = gulp.series(funcaoPadrao,dizOi)
 exports.default = gulp.parallel(funcaoPadrao,dizOi)
 exports.dizOi = dizOi
+exports.sass = compilaSass
+exports.watch = function() {  
+    gulp.watch('./source/styles/*.scss', {ignoreInitial:false} , gulp.series(compilaSass))
+}
